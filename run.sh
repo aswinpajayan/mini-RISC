d96d4d11d51fea2c235232687df4842b11098228
Aswin Ajayan <aswinp2008@gmail.com>
2019-04-13 16:41:50 +0530

    config changes

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 
 #run with the name of top level test bench module to analyse the wave forms
-echo "code is not complete just generated the initial P and G values"
 ./make.sh `find *.vhd | cut -d'.' -f1`
 gtkwave "$1.vcd"
 rm -f `find *.vcd | grep --invert "$1.vcd" `
