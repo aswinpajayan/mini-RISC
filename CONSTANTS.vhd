b844c3b97f5b4bc1ae4a6967bcabd1b28f65bf7d
Aswin Ajayan <aswinp2008@gmail.com>
2019-04-16 03:41:55 +0530

    added CONTROL_WORD_WIDTH

--- CONSTANTS.vhd
+++ CONSTANTS.vhd
@@ -3,4 +3,5 @@ package CONSTANTS is
 	constant GLOBAL_WIDTH : POSITIVE := 16;
 	constant I_MEM_SIZE   : POSITIVE := 64;
 	constant DATA_MEM_SIZE : POSITIVE := 64;
+	constant CONTROL_WORD_WIDTH : POSITIVE := 32;
 end package CONSTANTS;
