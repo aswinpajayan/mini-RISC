a35b87a1c8fed92abee282fc37ba18b510d81b0b
Aswin Ajayan <aswinp2008@gmail.com>
2019-04-15 12:23:55 +0530

    always write back PC .

--- register_file.vhd
+++ register_file.vhd
@@ -3,13 +3,17 @@ use IEEE.STD_LOGIC_1164.all;
 use IEEE.numeric_std.all;
 use IEEE.math_real.log2;
 use work.CONSTANTS.all;
-
+--we need an additional port to write back PC to R7
 entity register_file is generic(SIZE : POSITIVE :=8;
 				WIDTH: POSITIVE :=16);
 			port(read_address1,read_address2,write_address : in STD_LOGIC_VECTOR(integer(log2(real(SIZE)))-1 downto 0);
 				clk,write_en : in STD_LOGIC;
 				data_out1,data_out2 : out STD_LOGIC_VECTOR (WIDTH-1 downto 0);
-				data_in : in STD_LOGIC_VECTOR (WIDTH-1 downto 0));
+				data_in,PC_in : in STD_LOGIC_VECTOR (WIDTH-1 downto 0));
+
+--PC +1 should be always kept in last register in the register file  ,
+--R7 for a register file of size 8
+--no instruction should contain R7 as destination as it will be over-written
 end entity register_file;
 
 architecture rtl of register_file is
@@ -24,9 +28,9 @@ begin
 	 write_process: process(clk) begin
 		 if (rising_edge(clk) and write_en = '1') then
 			rblock(to_integer(unsigned('0' & write_address)))<= data_in;
+			rblock(SIZE -1 ) <= PC_in; --PC_in wrote in last location
 		 end if;
 	end process write_process;
-
 end architecture rtl;
 
 
