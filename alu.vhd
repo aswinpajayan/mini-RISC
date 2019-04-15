468091214134c0eb764bed2ebba24b1ebc8d9e70
Aswin Ajayan <aswinp2008@gmail.com>
2019-04-15 11:44:52 +0530

    removed compare operation from alu

--- alu.vhd
+++ alu.vhd
@@ -5,23 +5,21 @@ use IEEE.numeric_std.all;
 
 entity alu is 
 	generic(WIDTH: Natural :=16);
-	port(operand_1 : in std_logic_vector(WIDTH-1 downto 0);
-	     operand_2 : in std_logic_vector(WIDTH-1 downto 0);
-	 operation_sel : in STD_LOGIC_VECTOR(1 downto 0);
-    	     result :out std_logic_vector (WIDTH -1 downto 0);
+	port(operand_1 : in STD_LOGIC_VECTOR(WIDTH-1 downto 0);
+	     operand_2 : in STD_LOGIC_VECTOR(WIDTH-1 downto 0);
+	     operation_sel : in STD_LOGIC; -- 0 for addition 1 for nand
+    	     result :out STD_LOGIC_VECTOR (WIDTH -1 downto 0);
      		C,Z :out STD_LOGIC);
 end entity alu;
 architecture rtl of alu is
 signal temp_result : STD_LOGIC_VECTOR(WIDTH downto 0);
 begin
-	temp_result <=  std_logic_vector(((unsigned('0' & operand_1)) +  (unsigned('0' & operand_2)))) when operation_sel = "00" else	        	     ('0' & operand_1) nand  ('0' & operand_2) when operation_sel = "01" else  --nand
-			('0' & operand_1) xor   ('0' & operand_2) when operation_sel = "10" else -- compare BEQ
+	temp_result <=  std_logic_vector(((unsigned('0' & operand_1)) +  (unsigned('0' & operand_2)))) when operation_sel = '0' else	        	     ('0' & operand_1) nand  ('0' & operand_2) when operation_sel = '1' else --nand
 			(others => '0');
-	result <= temp_result(WIDTH-1 downto 0) when not ((operation_sel = "11") or (operation_sel = "10"));
+	result <= temp_result(WIDTH-1 downto 0) when operation_sel = '0';
 	--dont update result on compare
-	C <= temp_result(WIDTH) when operation_sel = "00" else 
-	     '0' ;
-	Z <= '1' when ((unsigned(temp_result) =0) and not(operation_sel = "11"));
+	C <= temp_result(WIDTH) when operation_sel = '0';
+	Z <= '1' when ((unsigned(temp_result) =0) and ((operation_sel = '0') or (operation_sel = '1')));
 
 end rtl;
 
