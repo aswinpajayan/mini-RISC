5bfe7dce6ed12a8c80b36c894111872c20688763
Aswin Ajayan <aswinp2008@gmail.com>
2019-04-21 19:44:26 +0530

    fixed a bug in test script , ADD,LW working fine

--- sim_pipeline.tcl
+++ sim_pipeline.tcl
@@ -15,29 +15,28 @@ sim:/pipeline/PC_value
 add wave -position insertpoint  \
 sim:/pipeline/REGISTER_BLOCK/RF/rblock
 
-add list -hexadecimal *
+add list -hexadecimal PIPE_REG_*
 force -freeze sim:/pipeline/FETCH_BLOCK/I_MEMORY/rblock(0)  0011001001000000 0	
 force -freeze sim:/pipeline/FETCH_BLOCK/I_MEMORY/rblock(1)  0011000010000000 0	
 force -freeze sim:/pipeline/FETCH_BLOCK/I_MEMORY/rblock(2)  0000001000010000 0	
-force -freeze sim:/pipeline/FETCH_BLOCK/I_MEMORY/rblock(3)  0101100101010000 0	
-force -freeze sim:/pipeline/FETCH_BLOCK/I_MEMORY/rblock(4)  0100110101010000 0	
-force -freeze sim:/pipeline/FETCH_BLOCK/I_MEMORY/rblock(5)  0000001000010000 0	
-force -freeze sim:/pipeline/FETCH_BLOCK/I_MEMORY/rblock(6)  0000001000010000 0	
-force -freeze sim:/pipeline/FETCH_BLOCK/I_MEMORY/rblock(7)  0000001000010000 0	
+force -freeze sim:/pipeline/FETCH_BLOCK/I_MEMORY/rblock(3)  0101010001010000 0	
+force -freeze sim:/pipeline/FETCH_BLOCK/I_MEMORY/rblock(4)  0101100100010000 0	
+force -freeze sim:/pipeline/FETCH_BLOCK/I_MEMORY/rblock(5)  0101010010010000 0	
+force -freeze sim:/pipeline/FETCH_BLOCK/I_MEMORY/rblock(6)  0101100101010000 0	
+force -freeze sim:/pipeline/FETCH_BLOCK/I_MEMORY/rblock(7)  0100010010010000 0	
 force -freeze sim:/pipeline/FETCH_BLOCK/I_MEMORY/rblock(8)  0000001000010000 0	
 force -freeze sim:/pipeline/FETCH_BLOCK/I_MEMORY/rblock(9)  0000001000010000 0	
 force -freeze sim:/pipeline/FETCH_BLOCK/I_MEMORY/rblock(10) 0000001000010000 0	
 force -freeze sim:/pipeline/FETCH_BLOCK/I_MEMORY/rblock(11) 0000001000010000 0	
 force -freeze sim:/pipeline/FETCH_BLOCK/I_MEMORY/rblock(12) 0000001000010000 0	
+force -freeze sim:/pipeline/FETCH_BLOCK/I_MEMORY/rblock(13) 0000001000010000 0	
 force -freeze sim:/pipeline/FETCH_BLOCK/I_MEMORY/rblock(14) 0000001000010000 0	
+force -freeze sim:/pipeline/FETCH_BLOCK/I_MEMORY/rblock(15) 0000001000010000 0	
+force -freeze sim:/pipeline/FETCH_BLOCK/I_MEMORY/rblock(16) 0000001000010000 0	
 
 force -freeze sim:/pipeline/RESET_IN 1 0
 run
 force -freeze sim:/pipeline/RESET_IN 0 1
 force -freeze sim:/pipeline/clk 1 0, 0 {50 ps} -r 100  				
-run 1200
-run 100
-force -freeze sim:/pipeline/RESET_IN 0 1
-force -freeze sim:/pipeline/clk 1 0, 0 {50 ps} -r 100  				
-run 1200
+run 
 
