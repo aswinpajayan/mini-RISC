4e6734b92cdf20b9477629fae8d40b3f23f807b1
Prashant_Sharma <49613869+prashant-iitb@users.noreply.github.com>
2019-04-22 06:14:54 +0530

    Update generic_register.vhd

--- generic_register.vhd
+++ generic_register.vhd
@@ -15,12 +15,13 @@ process (clk,clear) begin
 	if(rising_edge(clk)) then
 		if clear = '0' then
 			single_reg <= data_in ;
+			if(clear = '1') then 
+		single_reg <= (others => '0'); --asynchronous clear
+	end if;
 
 		end if;
 	end if;
-	if(clear = '1') then 
-		single_reg <= (others => '0'); --asynchronous clear
-	end if;
+	
 end process;
 end behav;
 
